d35cd4fd9ce248e5d49705842d08ccaf311c7ee3
PizzaGateTools <cmos01@sigaint.org>
2016-12-09 12:32:49 -0600

    Update pgate_screenshot.py: Added termcolor.
    Added termcolor and ascii art. Can also now be used with proxychains!

--- pgate_screenshot.py
+++ pgate_screenshot.py
@@ -1,6 +1,17 @@
 import screenshot
+from termcolor import colored, cprint
+print colored('   |  |    _ \ _)                ___|       |        ', 'red')
+print colored('_  |_ |_| |   | |_  /_  /  _` | |      _` | __|  _ \ ', 'white')
+print colored('_  |_ |_| ___/  |  /   /  (   | |   | (   | |    __/ ', 'white')
+print colored('  _| _|  _|    _|___|___|\__,_|\____|\__,_|\__|\___| ', 'blue')
+print colored('---------------------------------------------------- ', 'white')
+print colored(' Usage: python pgate_screenshot.py or 	            ', 'red')
+print colored(' proxychains python pgate_screenshot.py              ', 'red')
+print colored('---------------------------------------------------- ', 'red')
+print ('')
+
+
 form = 'http://'
 s = screenshot.Screenshot()
-image = s.get_image(form+raw_input("Enter the url you would like to capture: "))
-image.save(raw_input("Filename to save the image under: "))
-
+image = s.get_image(form+raw_input("Enter the url you would like to capture(ex.:google.com, facebook.com):: "))
+image.save(raw_input("Filename to save the image under: "))s
