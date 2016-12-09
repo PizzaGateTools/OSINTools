9865af2f822158bd911648649d77bd1eefb106fb
PizzaGateTools <cmos01@sigaint.org>
2016-12-09 11:09:15 -0600

    Update pgate_screenshot.py
    Fixed: No longer needs fully formed url. ex. 'http://www.google.com' vs. 'google.com'

--- pgate_screenshot.py
+++ pgate_screenshot.py
@@ -1,5 +1,6 @@
 import screenshot
+form = 'http://'
 s = screenshot.Screenshot()
-image = s.get_image(raw_input("Enter the url you would like to capture: "))
+image = s.get_image(form+raw_input("Enter the url you would like to capture: "))
 image.save(raw_input("Filename to save the image under: "))
 
